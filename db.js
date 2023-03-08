d5d49caa24d9bfd9738d046c89e3c45314c9be48
AKSRathor <98573681+AKSRathor@users.noreply.github.com>
2023-03-08 16:46:02 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,23 +1,13 @@
 require("dotenv").config();
-const express = require('express')
 
-// const mongoose = require('mongoose')
 const mongoose = require("mongoose")
-// const { default: mongoose } = require("mongoose")
-const password = "BQrStvgn3gRZJwzf"
-// const mongoURI = "mongodb://127.0.0.1:27017/portfolio"
-const uri = `mongodb+srv://aksr2003:${password}@portfolio.qrnpgxw.mongodb.net/`
-// const uri = process.env.MONGO_URI
-// const urc = (uri + "/?retryWrites=true&w=majority").toString()
-console.log(typeof uri)
-// const uri = process.env.MONGOLAB_URI
-// const uri = process.env.URI + "?retryWrites=true&w=majority"
-// console.log("THe code is ",urc)
-// mongodb+srv://aksr2003:$BQrStvgn3gRZJwzf@portfolio.qrnpgxw.mongodb.net/?retryWrites=true&w=majority
+const mongoURI = "mongodb://127.0.0.1:27017/portfolio"
+const uri = process.env.MONGO_URI + "?retryWrites=true&w=majority"
+
 const connectToMongoose =()=>{
-    // mongoose.set('strictQuery', true);
+    
     try { 
-        mongoose.connect(uri, ()=>{
+        mongoose.connect(uri || mongoURI, ()=>{
             console.log(uri)
             console.log("connected to mongoose")
         })
@@ -25,7 +15,6 @@ const connectToMongoose =()=>{
         console.log(error)
         process.exit()
     }
-    // .catch(err => console.log(err))
 
 }
 
